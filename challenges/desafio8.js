79a5745c657b274dcd358009c3577f94ac0e7f75
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-20 20:41:40 -0500

    feat : requsito 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,3 @@
+// 8 - Delete os lanches com menos de 50 curtidas e retorne o nome dos lanches que restaram no bancoco
+db.produtos.deleteMany({ curtidas: { $lt: 50 } });
+db.produtos.find({}, { nome: 1, _id: 0 });
