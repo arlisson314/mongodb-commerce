74463f7c3aaa712fa3d749d1fd50e9bf9c4f8b9a
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-22 12:08:21 -0500

    feat: requisito 30

---
+++ challenges/desafio30.js
@@ -0,0 +1,8 @@
+// 30 - Remova o campo curtidas do item Big Mac
+// Para isso, escreva no arquivo desafio30.js duas queries, nesta ordem:
+
+// Crie uma query que faça a remoção do campo curtidas do item Big Mac.
+
+// Crie uma query que retorne o nome para todos os documentos e curtidas (exceto para Big Mac).
+db.produtos.updateOne({ nome: "Big Mac" }, { $unset: { curtidas: "" } });
+db.produtos.find({}, { nome: 1, curtidas: 1, _id: 0 });
